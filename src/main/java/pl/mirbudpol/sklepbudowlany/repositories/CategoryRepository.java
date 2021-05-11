f73181fb31214bac6ecff68ab44ac86561f4145b
PrzemyslawStruminski <struminski.przemyslaw@gmail.com>
2021-05-11 20:09:32 +0200

    Added Category CRUD

--- src/main/java/pl/mirbudpol/sklepbudowlany/repositories/CategoryRepository.java
+++ src/main/java/pl/mirbudpol/sklepbudowlany/repositories/CategoryRepository.java
@@ -3,11 +3,14 @@ package pl.mirbudpol.sklepbudowlany.repositories;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import pl.mirbudpol.sklepbudowlany.entities.Category;
+import pl.mirbudpol.sklepbudowlany.entities.Thing;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository  extends CrudRepository<Category, Long>, PagingAndSortingRepository<Category, Long> {
 
     Optional<Category> findById(Long id);
+    List<Category> findAll();
     Category findByNazwaKategorii(String nazwaKategorii);
 }
--- src/main/java/pl/mirbudpol/sklepbudowlany/services/CategoryService.java
+++ src/main/java/pl/mirbudpol/sklepbudowlany/services/CategoryService.java
@@ -4,8 +4,17 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import pl.mirbudpol.sklepbudowlany.DTO.CategoryDTO;
+import pl.mirbudpol.sklepbudowlany.DTO.RatingDTO;
 import pl.mirbudpol.sklepbudowlany.entities.Category;
+import pl.mirbudpol.sklepbudowlany.entities.Client;
+import pl.mirbudpol.sklepbudowlany.entities.Rating;
+import pl.mirbudpol.sklepbudowlany.entities.Thing;
+import pl.mirbudpol.sklepbudowlany.exceptions.ResourceNotFoundException;
 import pl.mirbudpol.sklepbudowlany.repositories.CategoryRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -23,7 +32,29 @@ public class CategoryService {
         return categoryRepository.save(category);
     }
 
+    @Transactional
+    public void deleteCategory(Long id) {
+        categoryRepository.deleteById(id);
+    }
 
+    public List<CategoryDTO> getCategories(){
+
+        List<CategoryDTO> categoriesDTO = new ArrayList<>();
+        List<Category> categories = categoryRepository.findAll();
+
+        for(Category category : categories){
+            CategoryDTO categoryDTO = new CategoryDTO();
+            categoryDTO.setId(category.getId());
+            categoryDTO.setNazwaKategorii(category.getNazwaKategorii());
+            categoriesDTO.add(categoryDTO);
+        }
+        return categoriesDTO;
+    }
+
+    @Transactional
+    public void updateCategory(CategoryDTO dto) {
+        Optional <Category> category = categoryRepository.findById(dto.getId());
+    }
 
 
 }
