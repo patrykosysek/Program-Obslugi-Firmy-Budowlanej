515eaf4e2622b6bb2403d5f9e6e71231d9282c29
PrzemyslawStruminski <struminski.przemyslaw@gmail.com>
2021-05-25 17:06:17 +0200

    Added getRole endpoint, changes to security configuration

--- src/main/java/pl/mirbudpol/sklepbudowlany/additionalClasses/SecurityConfig.java
+++ src/main/java/pl/mirbudpol/sklepbudowlany/additionalClasses/SecurityConfig.java
@@ -6,10 +6,12 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import pl.mirbudpol.sklepbudowlany.services.CustomUserDetailsService;
 
 @EnableWebSecurity
@@ -47,6 +49,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .sessionFixation().migrateSession()
+                .and()
                 .authorizeRequests()
                 .antMatchers().permitAll()
                 .anyRequest().permitAll()
--- src/main/java/pl/mirbudpol/sklepbudowlany/controllers/ClientController.java
+++ src/main/java/pl/mirbudpol/sklepbudowlany/controllers/ClientController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import pl.mirbudpol.sklepbudowlany.DTO.EmailDTO;
 import pl.mirbudpol.sklepbudowlany.DTO.RatingDTO;
 import pl.mirbudpol.sklepbudowlany.DTO.RegisteredClientDTO;
 import pl.mirbudpol.sklepbudowlany.services.ClientService;
@@ -59,5 +60,12 @@ public class ClientController {
     @ResponseStatus(HttpStatus.OK)
     public void deleteManager(@PathVariable Long menagoId, @PathVariable Long userId) {
         clientService.deleteManager(menagoId, userId);
+    }
+
+    @ApiOperation("Endpoint pobrania roli użytkownika na podstawie jego emailu")
+    @PostMapping(path = "/getRole")
+    @ResponseStatus(HttpStatus.CREATED)
+    public int getRole(@Validated @RequestBody EmailDTO dto) {
+        return clientService.getRole(dto);
     }
 }
